8886e6784324f7ca3726e762a4d9793dced52a49
指原奈々 <>
2021-04-26 11:39:05 +0900

    navigationController付与 tableView背景設定のみ

---
+++ ChatAppWithFirebase/ChatListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatListViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by 指原奈々 on 2021/04/25.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
